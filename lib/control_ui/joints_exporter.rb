933215014a064111a12d3716d0ed96219b898126
Leon Hansen <Leon.Hansen@DFKI.de>
2015-09-17 17:30:37 +0200

    now Imports and Exports JointWaypointProvider Configs instead of Joint samples

--- lib/control_ui/joints_exporter.rb
+++ lib/control_ui/joints_exporter.rb
@@ -1,14 +1,67 @@
 require 'Qt'
 
-Orocos.load_typekit_for "/base/samples/Joints"
+Orocos.load_typekit "waypoint_provider"
 
 class JointsExporter < Qt::Widget
  
 	def initialize(ctrl_gui)
 		super(nil)
-		hlayout = Qt::HBoxLayout.new
+		vlayout = Qt::VBoxLayout.new
 		
-		file_dlg = Qt::FileDialog.new		
+		btnlayout = Qt::HBoxLayout.new
+		file_dlg = Qt::FileDialog.new
+		
+		tabs_widget = Qt::TabWidget.new
+		tmodel = Orocos::task_model_from_name "waypoint_provider::JointWaypointProvider"
+		tcfg = Orocos::TaskConfigurations.new(tmodel)
+		
+		def updateTabMenu(tabs_widget,tcfg,ctrl_gui)
+		    ind = tabs_widget.currentIndex()
+		    tabs_widget.clear
+		    tcfg.sections.each do |name,cfgmap|
+                radlayout = Qt::HBoxLayout.new
+                tabwid = Qt::Widget.new
+                tabwid.setLayout(radlayout)
+                counter = 1
+                if cfgmap["waypoints"] then
+                    cfgmap["waypoints"].each do |joint_sample|
+                        radbtn = Qt::RadioButton.new("State " + counter.to_s)
+                        counter += 1
+                        radbtn.connect(SIGNAL('clicked()')) do
+                            if radbtn.isChecked() then
+                                ctrl_gui.setReference(joint_sample)
+                            end
+                        end
+                        radlayout.addWidget(radbtn)                        
+                    end
+                    addwaybtn = Qt::PushButton.new("Add a Waypoint")
+                    addwaybtn.connect(SIGNAL('clicked()')) do
+                        joints = ctrl_gui.getJoints()
+                        cfgmap["accuracies"].push({"data"=>Array.new(joints.elements.size,0.01)})
+                        cfgmap["waypoints"].push(joints)
+                        updateTabMenu(tabs_widget,tcfg,ctrl_gui)
+                    end
+                    radlayout.addWidget(addwaybtn)
+                    tabs_widget.addTab(tabwid,name)
+                end
+                tabs_widget.setCurrentIndex(ind)
+            end
+            inplayout = Qt::HBoxLayout.new
+            adddocwid = Qt::Widget.new
+            textbox = Qt::LineEdit.new
+            inplayout.addWidget(textbox)
+            doc_btn = Qt::PushButton.new("Create Document")
+            doc_btn.connect(SIGNAL('clicked()')) do
+                new_conf = {"accuracies"=>[],"waypoints"=>[]}
+                tcfg.add(textbox.text,new_conf)
+                updateTabMenu(tabs_widget,tcfg,ctrl_gui)
+            end
+            inplayout.addWidget(doc_btn)
+            adddocwid.setLayout(inplayout)
+		    tabs_widget.addTab(adddocwid,"Add a Document")			
+		end
+		updateTabMenu(tabs_widget,tcfg,ctrl_gui)
+			            		
 		
 		impt_btn = Qt::PushButton.new("Import Joints Configuration")
 		impt_btn.connect(SIGNAL('clicked()')) do
@@ -16,22 +69,16 @@ class JointsExporter < Qt::Widget
 			if file_dlg.exec and file_dlg.result == Qt::Dialog::Accepted then
 				file_name = file_dlg.selectedFiles()[0]
 				if File.file?(file_name) then
-				    f = File.new(file_name,"r")
-				    joint_sample = Types::Base::Samples::Joints.new
 				    begin
-				        imp_map = YAML::load(f.read)				
-				        Orocos::TaskConfigurations.typelib_from_yaml_value joint_sample, imp_map
-                    rescue IOError,Psych::SyntaxError,Typelib::UnknownConversionRequested => errName
-                       puts "Import failed: " + errName.to_s
-                    else
-                        ctrl_gui.setReference(joint_sample)
+				        tcfg.load_from_yaml(file_name)
+				        updateTabMenu(tabs_widget,tcfg,ctrl_gui)
+                    rescue IOError,Psych::SyntaxError => errName
+                        puts "Import failed: " + errName.to_s
                     end
-                    f.close
                 end
 			end	
-		end
-		
-		hlayout.addWidget(impt_btn)
+		end		
+		btnlayout.addWidget(impt_btn)
 		
 		expt_btn = Qt::PushButton.new("Export Joints Configuration")
 		expt_btn.connect(SIGNAL('clicked()')) do
@@ -39,18 +86,38 @@ class JointsExporter < Qt::Widget
 			if file_dlg.exec and file_dlg.result == Qt::Dialog::Accepted then
 				file_name = file_dlg.selectedFiles()[0]
 				
-				if not File.directory?(file_name) then
-					sample = ctrl_gui.getJoints()
-					exp_yaml = Orocos::TaskConfigurations.typelib_to_yaml_value(sample).to_yaml
-					f = File.new(file_name,"w")
-					f.write(exp_yaml)
-					f.close
-				end
+				if File.exists?(file_name) then
+					File.delete(file_name)
+			    end
+
+				tcfg.sections.each do |name,cfgmap|
+				    expcontext = Orocos::RubyTasks::TaskContext.from_orogen_model("exporter_context",tmodel)
+				    #expcontext.each_property = {}.each
+				    tcfg.apply(expcontext,[name],true)
+			        if name != "default" and tcfg["default"] then
+				        expcontext.properties.each do |pname,pval|
+				            if tcfg.sections["default"][pname] == pval then
+				                
+				            end
+				        end
+				    end
+				    #puts expcontext.properties
+			        tcfg.save(expcontext,file_name,name)
+			    end
 			end
 		end
+		btnlayout.addWidget(expt_btn)
 		
-		hlayout.addWidget(expt_btn)
-
-		setLayout(hlayout)
+		
+		
+		tgl_btn = Qt::PushButton.new("Hide Tab Widget")
+		tgl_btn.connect(SIGNAL('clicked()')) do
+		    tabs_widget.setVisible(tabs_widget.isHidden)
+		end
+		btnlayout.addWidget(tgl_btn)
+		
+		vlayout.addWidget(tabs_widget)
+		vlayout.addItem(btnlayout)
+		setLayout(vlayout)
 	end
 end
